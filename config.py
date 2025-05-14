6659be6a867d8f063aa3fec13b545f9435d1dd9f
YIFAN WANG <y.wang5.24@abdn.ac.uk>
2025-05-14 18:45:39 +0000

    Updating config points to cybersecurity_events.db

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.sqlite3')
+        'sqlite:///' + os.path.join(basedir, 'cybersecurity_events.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
