3742eee4c4756d34a7911fc851ffed52519a046f
YIFAN WANG <y.wang5.24@abdn.ac.uk>
2025-05-14 20:18:18 +0000

    Add severity filter handling in events view

--- app/views.py
+++ app/views.py
@@ -1,18 +1,33 @@
-from flask import render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Data
-from flask import current_app as app
 
-@app.route('/')
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
 def index():
-    return redirect(url_for('events'))
+    return redirect(url_for('main.events'))
 
-@app.route('/events/')
+@bp.route('/events/')
 def events():
+    # get the severity == low/medium/high
+    severity = request.args.get('severity', type=str)
     page = request.args.get('page', 1, type=int)
-    pagination = Data.query.paginate(page, per_page=20)
-    return render_template('events_list.html', pagination=pagination)
 
-@app.route('/event/<string:event_id>/')
+    # basic searching
+    query = Data.query
+    if severity:
+        query = query.filter_by(attack_severity=severity.capitalize())
+
+    # into pages
+    pagination = query.paginate(page, per_page=20)
+
+    return render_template(
+        'events_list.html',
+        pagination=pagination,
+        current_severity=severity  # Template return for activating
+    )
+    
+@bp.route('/event/<string:event_id>/')
 def event_detail(event_id):
     data = Data.query.get_or_404(event_id)
     return render_template('event_detail.html', data=data, resp=data.response)
